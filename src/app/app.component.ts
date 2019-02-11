2f5c86437fd04cd7f94f8c977326fbf181ee1952
sarvya521 <Sarvesh.Padwal@experian.com>
2019-02-11 14:30:33 +0800

    bug-fix-recall-api-on-select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,13 @@ export class AppComponent {
    title = 'auto-complete-ui';
    searchCity :  FormControl = new FormControl();
    cities = <any>[];
+   currentCity;
    constructor(private apiService: ApiService) { }
 
    ngOnInit () {
       this.searchCity.valueChanges.subscribe(
          key => {
-           if (key != '') {
+           if (key != '' && this.doKeyExists(key) === false) {
              this.apiService.getCities(key).subscribe(
                data => {
                  this.cities = data as any[];
@@ -26,21 +27,16 @@ export class AppComponent {
            }
        })
    }
-   
-   //build list of states as map of key-value pairs
-   /*loadStates() {
-      var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
-         Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
-         Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
-         Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
-         North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
-         South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
-         Wisconsin, Wyoming';
-      this.states =  allStates.split(/, +/g).map( function (state) {
-         return {
-            value: state.toUpperCase(),
-            display: state
-         };
-      });
-   }*/
+
+   doKeyExists(key) : boolean {
+      if(this.cities.length > 0) {
+         return this.cities.some(function(c){ return c.name === key});
+      }
+      return false;
+   }
+
+   getSelectedCity(city){
+      this.currentCity = city;
+      console.log("current selected city:"+city.name);
+   }
 }
